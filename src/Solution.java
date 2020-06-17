7a824ab7ab04859fa1f0ed30c33faabea98f1b47
Victor <FilimonovVA.95@mail.ru>
2020-06-17 18:20:29 +0300

    fix ерÑthird task + veriашсÑification

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,51 @@
 public class Solution {
     public static void main(String[] args) {
 
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+        System.out.println();
 
+        System.out.println(sumDouble(1, 2));
+        System.out.println(sumDouble(3, 2));
+        System.out.println(sumDouble(2, 2));
+        System.out.println();
+
+        System.out.println(altPairs("kitten"));
+        System.out.println(altPairs("Chocolate"));
+        System.out.println(altPairs("CodingHorror"));
+        System.out.println();
 
     }
 
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
         if (weekday && !vacation)
             return false;
         else
             return true;
     }
 
-    public int sumDouble(int a, int b) {
+    public static int sumDouble(int a, int b) {
         if (a == b)
             return a*4;
         else
             return a + b;
     }
 
-    public String altPairs(String str) {
+    public static String altPairs(String str) {
         String result = "";
-        for (int i = 1; i < str.length(); i = i + 2) {
-            result += str.charAt(i-1) + str.charAt(i);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (((i / 2) % 2) == 0) {
+                result += str.charAt(i);
+            }
         }
+
         return result;
+
     }
+
+
 
 
 
